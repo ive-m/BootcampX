f9db6180043d7e658a0ff18f8a6045668d52e8d5
ive-m <ivesita.maria@gmail.com>
2023-01-11 13:25:44 -0500

    Add 4_queries

---
+++ 4_queries/teachers_did_assistance_req_during_a_cohort.sql
@@ -0,0 +1,6 @@
+SElECT teachers.name AS teacher, cohorts.name as cohort
+FROM teachers 
+JOIN assistance_requests ON teachers.id=assistance_requests.teacher_id
+JOIN students ON students.id= assistance_requests.student_id
+JOIN cohorts ON students.cohort_id=cohorts.id
+WHERE cohorts.name='JUL02';
---
+++ 4_queries/teachers_did_assistance_req_during_a_cohort_and_total_assistances.sql
@@ -0,0 +1,15 @@
+/*SElECT teachers.name AS teacher, cohorts.name as cohort, count(assistance_requests) AS total_assistances
+FROM teachers 
+JOIN assistance_requests ON teachers.id=assistance_requests.teacher_id
+JOIN students ON students.id= assistance_requests.student_id
+JOIN cohorts ON students.cohort_id=cohorts.id
+GROUP BY teachers.name, cohorts.name
+HAVING cohorts.name='FEB12';*/
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
